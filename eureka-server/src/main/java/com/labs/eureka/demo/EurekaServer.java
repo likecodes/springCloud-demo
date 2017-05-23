f3d1900c97c163548a1ec886186fc07d1579506e
leo <leo@uxunchina.net>
2017-05-23 18:07:09 +0800

    delete .idea

---
+++ eureka-server/src/main/java/com/labs/eureka/demo/EurekaServer.java
@@ -0,0 +1,17 @@
+package com.labs.eureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by leo on 2017/5/23.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class,args);
+    }
+}
+
