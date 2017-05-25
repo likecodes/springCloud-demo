f66d37d1f72b7bb6de257794f59277237ee3675c
leo <leo@uxunchina.net>
2017-05-25 12:51:20 +0800

    删废旧代码

--- actInsInfo-service/src/main/java/com/leo/labs/act/ins/service/ActInsInfoApplication.java
+++ actInsInfo-service/src/main/java/com/leo/labs/act/ins/service/ActInsInfoApplication.java
@@ -18,26 +18,7 @@ import java.util.*;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ActInsInfoApplication {
-    private static final String FACTORIES_RESOURCE_LOCATION="META-INF/spring.factories";
     public static void main(String[] args) {
-        //
-        String factoryClassName = ApplicationContextInitializer.class.getName();
-        try {
-            ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
-            Enumeration<URL> urls = (classLoader != null ? classLoader.getResources(FACTORIES_RESOURCE_LOCATION) :
-                    ClassLoader.getSystemResources(FACTORIES_RESOURCE_LOCATION));
-            List<String> result = new ArrayList<String>();
-            while (urls.hasMoreElements()) {
-                URL url = urls.nextElement();
-                Properties properties = PropertiesLoaderUtils.loadProperties(new UrlResource(url));
-                String factoryClassNames = properties.getProperty(factoryClassName);
-                result.addAll(Arrays.asList(StringUtils.commaDelimitedListToStringArray(factoryClassNames)));
-            }
-        }
-        catch (IOException ex) {
-            throw new IllegalArgumentException("Unable to load ["  +
-                    "] factories from location [" + FACTORIES_RESOURCE_LOCATION + "]", ex);
-        }
         SpringApplication.run(ActInsInfoApplication.class,args);
     }
 }
